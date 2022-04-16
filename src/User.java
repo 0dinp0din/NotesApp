6d0c4f0e141dffe0bc71a6ec449a7711836b4809
odin <odin481@gmail.com>
2022-04-16 16:57:08 +0200

    Progress on the user superclass
    - Added a check for if a note-title exists
    - Implemented the "printnote" method
    - Finished the option method, and put it in a while loop with a simple exit condition.

--- src/User.java
+++ src/User.java
@@ -34,15 +34,22 @@ public class User {
 
         System.out.println("What should the title of your note be?");
         String title = scanner.next();
-        System.out.println("What is the contents of your note?");
-        String content = scanner.next();
 
-        variables.put(1, String.valueOf(id));
-        variables.put(2, title);
-        variables.put(3, content);
-        database.update(variables, query);
+        if (!titleExists(title)) {
+            System.out.println("What is the contents of your note?");
+            String content = scanner.next();
 
-        variables.clear();
+            variables.put(1, String.valueOf(id));
+            variables.put(2, title);
+            variables.put(3, content);
+            database.update(variables, query);
+
+            variables.clear();
+        } else if (titleExists(title)) {
+            System.out.printf("A note with the title \"%s\" already exists.", title);
+        } else {
+            System.out.println("An unknown error has occurred..");
+        }
 
     }
 
@@ -50,9 +57,16 @@ public class User {
 
     }
 
-    public HashMap<Integer, String> listAllNotes() throws SQLException {
+    public boolean titleExists(String title) throws SQLException {
+        this.query = "SELECT note_title FROM notes WHERE note_title = ?;";
+        variables.put(1, title);
+        ResultSet rs = database.fetch(variables, query);
+        return rs.next();
+    }
+
+    public HashMap<String, String> listAllNotes() throws SQLException {
         int index = 0;
-        HashMap<Integer, String> dboutput = new HashMap<>();
+        HashMap<String, String> dboutput = new HashMap<>();
 
         this.query = "SELECT note_title FROM notes WHERE id = ?;";
         variables.put(1, String.valueOf(id));
@@ -61,41 +75,61 @@ public class User {
         if (!result.isBeforeFirst()){
             System.out.println("You don't have any saved notes.");
             return null;
+        }else {
+            System.out.println("Your saved notes are:");
         }
         while (result.next()){
             index++;
             System.out.println(index+ ". " + result.getString("note_title"));
-            dboutput.put(index, result.getString("note_title"));
+            dboutput.put(String.valueOf(index), result.getString("note_title"));
         }
         variables.clear();
         return dboutput;
     }
 
+    public void printNote() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        HashMap<String, String> list = listAllNotes();
+        System.out.println("Type the number of the note you would like to print:");
+        String selection = input.next();
+
+        query = "SELECT note_content FROM notes WHERE id = ? AND note_title = ?;";
+        variables.put(1, String.valueOf(this.id));
+        variables.put(2, list.get(selection));
+        ResultSet result = database.fetch(variables, query);
+
+        while (result.next()){
+            System.out.println(result.getString("note_content"));
+        }
+    }
+
 
     public void option() throws SQLException {
-        System.out.println("""
+        boolean exit = false;
+
+        while (!exit){
+            System.out.println("""
                         Please select an action below:
                         1. Create a new note
                         2. Delete a note
                         3. Print out a note
-                        4. List all notes""");
+                        4. Exit application""");
 
-        Scanner scanner = new Scanner(System.in);
-        String option = scanner.next();
+            Scanner scanner = new Scanner(System.in);
+            String option = scanner.next();
 
-        switch (option) {
-            case "1" -> {
-                addNote();
+            switch (option) {
+                case "1" -> {
+                    addNote();
+                }case "2" -> {
+
+                }case "3" -> {
+                    printNote();
+                }case "4" -> {
+                    System.out.println("Goodbye!");
+                    exit = true;
+                }
             }
-            case "4" -> {
-                listAllNotes();
-            }
-
         }
-//        if (option.equals("1")){
-//            addNote();
-//        } else if (option.equals("4")) {
-//            listAllNotes();
-//        }
     }
 }
